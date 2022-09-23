f3820d85a074eac35885ac30728787c2fdab04bb
Martin Agüero Trevisan <aguerotrevisanmartin@gmail.com>
2022-09-23 17:28:33 -0300

    Pruebas

--- Cuenta bancaria/Program.cs
+++ Cuenta bancaria/Program.cs
@@ -12,6 +12,19 @@ class Program {
 
         cPesos.Extraccion(300000,TipoDeExtraccion.CajeroHumano);
 
+        Console.WriteLine("Creo una lista de cuentas:");
+
+        var lista = new List<Cuenta>();
+
+        lista.Add(cPesos);
+        lista.Add(new CuentaDolares(2000,"Sofia Perez","45493482"));
+        lista.Add(new CajaAhorroPesos(20000,"José Juarez","43523423"));
+
+        foreach (var cuenta in lista)
+        {
+            cuenta.mostrarInformacionCuenta();
+        }
+
         Console.ReadLine();
 
         return 0;
